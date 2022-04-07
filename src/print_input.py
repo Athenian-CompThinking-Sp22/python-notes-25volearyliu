439856dbca88743ada9c5b434bded92c69bedb65
violetolearyliu <25volearyliu@athenian.org>
2022-04-07 09:04:27 -0700

    Day 1

--- src/print_input.py
+++ src/print_input.py
@@ -6,3 +6,11 @@
 #############################
 
 # Write code here:
+print("HI FRANKIE :)")
+
+name = input("Please enter your name: ")
+print(f'Hi {name} :)')
+
+num1 = 100
+num2 = 12
+print(num1/num2)
